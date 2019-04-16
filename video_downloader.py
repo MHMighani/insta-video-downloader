f0b20af2a56a08b6241c60543558ef06cdb751f9
Hermes223 <mh.mgh2012@gmail.com>
2019-04-16 15:36:11 +0430

    name of video problem solved

--- video_downloader.py
+++ video_downloader.py
@@ -2,12 +2,21 @@ import urllib.request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-url = input("Please enter url_link> ")
-driver = webdriver.Firefox(executable_path=r'/home/hermes/Desktop/insta_video_downloader/geckodriver')
-driver.get(url)
-elementName = driver.find_elements_by_class_name("tWeCl");
-src_url = elementName[0].get_attribute("src")
 
-url_link = src_url
-urllib.request.urlretrieve(url_link,'video_name.mp4')
-driver.close()
+def video_downloader(url):
+	driver = webdriver.Firefox(executable_path=r'/home/hermes/Desktop/insta_video_downloader/geckodriver')
+	driver.get(url)
+	elementName = driver.find_elements_by_class_name("tWeCl");
+	src_url = elementName[0].get_attribute("src")
+	video_name = (url.split('/'))[4]
+	url_link = src_url
+	urllib.request.urlretrieve(url_link,video_name + '.mp4')
+	driver.close()
+
+url = input("Please enter url_link> ")
+
+video_downloader(url)	
+
+if __name__ == '__main__':
+	pass
+
